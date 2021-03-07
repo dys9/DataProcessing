cdf666d1418b3b3c68ab68d56b6a47a63eff62ae
dys9 <dys9@naver.com>
2021-03-07 22:18:05 +0900

    2021.03.07, (1)

---
+++ NSCAL_CLIB/UserControl1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace NSCAL_CLIB
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 구성 요소 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.OPD = new System.Windows.Forms.OpenFileDialog();
+            this.tbX = new System.Windows.Forms.TextBox();
+            this.tbY = new System.Windows.Forms.TextBox();
+            this.tbValue = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(4, 4);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.ReadOnly = true;
+            this.tbPath.Size = new System.Drawing.Size(314, 24);
+            this.tbPath.TabIndex = 0;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(324, 3);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(83, 24);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open..";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // OPD
+            // 
+            this.OPD.FileName = "openFileDialog1";
+            // 
+            // tbX
+            // 
+            this.tbX.Location = new System.Drawing.Point(4, 34);
+            this.tbX.Name = "tbX";
+            this.tbX.Size = new System.Drawing.Size(100, 24);
+            this.tbX.TabIndex = 2;
+            this.tbX.TextChanged += new System.EventHandler(this.tbX_TextChanged);
+            // 
+            // tbY
+            // 
+            this.tbY.Location = new System.Drawing.Point(110, 34);
+            this.tbY.Name = "tbY";
+            this.tbY.Size = new System.Drawing.Size(100, 24);
+            this.tbY.TabIndex = 3;
+            this.tbY.TextChanged += new System.EventHandler(this.tbY_TextChanged);
+            // 
+            // tbValue
+            // 
+            this.tbValue.Location = new System.Drawing.Point(216, 34);
+            this.tbValue.Name = "tbValue";
+            this.tbValue.ReadOnly = true;
+            this.tbValue.Size = new System.Drawing.Size(100, 24);
+            this.tbValue.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(4, 62);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(793, 385);
+            this.textBox1.TabIndex = 4;
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbValue);
+            this.Controls.Add(this.tbY);
+            this.Controls.Add(this.tbX);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.tbPath);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(800, 450);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.OpenFileDialog OPD;
+        private System.Windows.Forms.TextBox tbX;
+        private System.Windows.Forms.TextBox tbY;
+        private System.Windows.Forms.TextBox tbValue;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ NSCAL_CLIB/UserControl1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NSCAL_CLIB
+{
+    public partial class UserControl1 : UserControl
+    {
+        private int L, R, U, D, Xmax, Ymax = 0;
+        private double Xstep, Ystep, Flux = 0.0;
+        private double[,] arr;
+
+
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private double Bi_Interpolation(double X, double Y)
+        {
+            double result = 0;
+            // 보간이 필요없을 경우
+            if (((X - D) % Xstep) == 0 && ((Y - L) % Ystep) == 0)
+            {
+                int xi = (int)((X - D) / Xstep);
+                int yi = (int)((Y - L) / Ystep);
+
+                result = (arr[xi, yi]);
+            }
+            // X 보간
+            else if (((X - D) % Xstep) != 0 && ((Y - L) % Ystep) == 0)
+            {
+                int xmi = (int)((X - D) / Xstep);
+                int ymi = (int)((Y - L) / Ystep);
+                int xMi = xmi + 1;
+
+                double t = (X % Xstep) / Xstep;
+
+                if (xmi > 0 && xmi < Xmax &&
+                    xMi > 0 && xMi < Xmax &&
+                    ymi > 0 && ymi < Ymax)
+                {
+                    double valm = arr[xmi, ymi];
+                    double valM = arr[xMi, ymi];
+
+                    result = valm * (1 - t) + t * valM;
+                }
+            }
+            // Y 보간
+            else if (((X - D) % Xstep) == 0 && ((Y - L) % Ystep) != 0)
+            {
+                int xmi = (int)((X - D) / Xstep);
+                int ymi = (int)((Y - L) / Ystep);
+                int yMi = xmi + 1;
+
+                double t = (Y % Xstep) / Ystep;
+
+                if (xmi > 0 && xmi < Xmax &&
+                    ymi > 0 && ymi < Ymax &&
+                    yMi > 0 && yMi < Ymax
+                    )
+                {
+                    double valm = arr[xmi, ymi];
+                    double valM = arr[xmi, yMi];
+
+                    result = valm * (1 - t) + t * valM;
+                }
+            }
+            // 이중 보간
+            else
+            {
+                int xmi = (int)((X - D) / Xstep);
+                int ymi = (int)((Y - L) / Ystep);
+
+                int xMi = xmi + 1;
+                int yMi = ymi + 1;
+
+                if (xmi > 0 && xmi < Xmax &&
+                     xMi > 0 && xMi < Xmax &&
+                     ymi > 0 && ymi < Ymax &&
+                     yMi > 0 && yMi < Ymax)
+                {
+                    double valm = arr[xmi, ymi];
+                    double valM = arr[xMi, yMi];
+
+                    result = 0;
+                }
+            }
+            return result;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            tbPath.Clear();
+            OPD.InitialDirectory = "C:\\Users\\dys9\\Desktop";
+
+            if (OPD.ShowDialog() == DialogResult.OK)
+            {
+                tbPath.Text = OPD.FileName;
+            }
+
+            if (tbPath.Text != "")
+            {
+                try
+                {
+                    using (StreamReader SR = new StreamReader(tbPath.Text))
+                    {
+                        int Xidx = 0;
+                        int Yidx = 0;
+                        string line = "";
+
+                        // Parse Header 
+                        if ((line = SR.ReadLine()) != null)
+                        {
+                            string[] header = line.Split(new char[] { ' ', '\t' });
+                            if (header.Length == 7)
+                            {
+                                U = Convert.ToInt32(header[0]);
+                                D = Convert.ToInt32(header[1]);
+                                Xstep = Convert.ToDouble(header[2]);
+                                R = Convert.ToInt32(header[3]);
+                                L = Convert.ToInt32(header[4]);
+                                Ystep = Convert.ToDouble(header[5]);
+                                Flux = Convert.ToDouble(header[6]);
+                            }
+                            else
+                            {
+                                MessageBox.Show("Input Header Error!");
+                            }
+                            Xmax = (int)((U - D) / Xstep) + 1;
+                            Ymax = (int)((R - L) / Ystep) + 1;
+
+                            textBox1.Text += String.Format("[({0}, {1})], " +
+                                "[X : ({3} ~ {2}) , {4}], " +
+                                "[Y : ({6} ~ {5}) , {7}], " +
+                                "[Flux : {8}]\r\n\r\n",
+                                Xmax, Ymax, U, D, Xstep, R, L, Ystep, Flux);
+
+                            arr = new double[Xmax, Ymax];
+                        }
+
+                        // Insert Data to Array
+                        while ((line = SR.ReadLine()) != null)
+                        {
+                            if (((Xidx < Xmax) && (Yidx < Ymax)))
+                            {
+                                textBox1.Text += String.Format("{0,5}", Convert.ToDouble(line));
+                                arr[Xidx, Yidx] = Convert.ToDouble(line);
+                            }
+                            else
+                            {
+                                break;
+                            }
+
+                            Yidx++;
+
+                            if (Yidx == Ymax)
+                            {
+                                Xidx++;
+                                Yidx = 0;
+                                textBox1.Text += "\r\n";
+                            }
+                        }
+
+                        // Throw Exception, If header's not match.
+                        if (line == null)
+                        {
+                            throw new Exception();
+                        }
+                        SR.Close();
+                    }
+                } // Exceptions
+                catch (IOException)
+                {
+                    MessageBox.Show("IOException");
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    MessageBox.Show("IndexOutOfRangeException");
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("InputDataIndexException");
+                }
+            }
+        }
+
+        private void tbY_TextChanged(object sender, EventArgs e)
+        {
+            double X, Y;
+            if (arr != null)
+            {
+                if (double.TryParse(tbY.Text, out Y) && double.TryParse(tbX.Text, out X))
+                {
+                    if ((X <= U && X >= D) && (Y <= R && Y >= L))
+                    {
+                        tbValue.Text = Bi_Interpolation(X, Y).ToString();
+                    }
+                }
+            }
+        }
+
+        private void tbX_TextChanged(object sender, EventArgs e)
+        {
+            double X, Y;
+            if (arr != null)
+            {
+                if (double.TryParse(tbY.Text, out Y) && double.TryParse(tbX.Text, out X))
+                {
+                    if ((X <= U && X >= D) && (Y <= R && Y >= L))
+                    {
+                        tbValue.Text = Bi_Interpolation(X, Y).ToString();
+                    }
+                }
+            }
+        }
+    }
+}
